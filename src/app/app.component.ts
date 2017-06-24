d3753d5ed168ad65611b38ca9aaf1d6dd9e386d4
Sascha Rose <sascha.rose@gmail.com>
2017-06-24 14:23:32 +0200

    backup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,22 +16,75 @@ export class AppComponent implements OnInit {
 
   constructor(private http: Http) {}
 
-  ngOnInit() {
-    const url = API_PATH + '/lights';
-    this.http.get(url)
-      .subscribe(response => this.response = response.json(), 
-      err => console.log('error during http request: ', url, err), 
-      () => {
-        console.log('received response from: ', url);
-        this.items = _.values(this.response);
-      }
-    );
-
+  loadData() {
+    return new Promise((resolve, reject) => { 
+      const url = API_PATH + '/lights';
+      this.http.get(url)
+        .subscribe(response => this.response = response.json(), 
+        err => {
+          console.log('error during http request: ', url, err)
+          reject(err);
+        }, 
+        () => {
+          console.log('received response from: ', url);
+          resolve(_.values(this.response));
+        }
+      )
+    });
   }
 
+  initSortOrder() {
+    this.changeSortOrder('name');
+  }
 
+  changeSortOrder(orderby: string) {
+    switch (orderby) {
+      case 'state': 
+        this.items = _.sortBy(this.items, 'state.on');
+        break;
+      default:
+        this.items = _.sortBy(this.items, 'name');
+    }
+  }
 
+  ngOnInit() {
+    this.loadData()
+      .then(data => this.items = data)
+      .then(() => this.initSortOrder()); 
+  }
 
+  onRefresh() {
+    this.loadData()
+      .then(data => this.items = data)
+      .then(() => this.initSortOrder()); 
+  }
+
+  onTurnOn(item: any) {
+    return new Promise((resolve, reject) => { 
+    console.log(item);
+    const url = API_PATH + '/lights/' + item.uniqueid + '/state';
+    this.http.put(url, { on: true })
+        .subscribe(response => this.response = response.json(), 
+        err => {
+          console.log('error during http request: ', url, err)
+          reject(err);
+        }, 
+        () => {
+          console.log('received response from: ', url, ' response: ', this.response);
+          
+          resolve(_.values(this.response));
+        }
+      )
+    })
+  }
+
+  onTurnOff(item: any) {
+    console.log(item);
+  }
+
+  onDetails(item: any) {
+    console.log(item);
+  }
 }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule, 
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
