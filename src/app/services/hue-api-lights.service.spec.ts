e93b0a31238acf3631e3f2812539f45858a7c2cd
Sascha Rose <sascha.rose@gmail.com>
2017-06-25 14:28:46 +0200

    tried to get unit tests working without success

--- src/app/services/hue-api-lights.service.spec.ts
+++ src/app/services/hue-api-lights.service.spec.ts
@@ -1,15 +1,56 @@
-import { TestBed, inject } from '@angular/core/testing';
-
+import { TestBed, async, inject } from '@angular/core/testing';
+import {
+  HttpModule,
+  Http,
+  Response,
+  ResponseOptions,
+  XHRBackend
+} from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 import { HueApiLightsService } from './hue-api-lights.service';
 
 describe('HueApiLightsService', () => {
+
   beforeEach(() => {
+
     TestBed.configureTestingModule({
-      providers: [HueApiLightsService]
+      imports: [HttpModule],
+      providers: [
+        HueApiLightsService,
+        { provide: XHRBackend, useClass: MockBackend },
+      ]
     });
   });
 
-  it('should ...', inject([HueApiLightsService], (service: HueApiLightsService) => {
-    expect(service).toBeTruthy();
-  }));
+  describe('getAllLights()', () => {
+
+    it('should return an Observable<Array<any>>',
+        inject([HueApiLightsService, XHRBackend], (hueApiLightsService, mockBackend) => {
+
+        const mockResponse = {
+          data: [
+            { id: 0, name: 'Light 0' },
+            { id: 1, name: 'Light 1' },
+            { id: 2, name: 'Light 2' },
+            { id: 3, name: 'Light 3' },
+          ]
+        };
+
+        mockBackend.connections.subscribe((connection) => {
+          connection.mockRespond(new Response(new ResponseOptions({
+            body: JSON.stringify(mockResponse)
+          })));
+        });
+
+        hueApiLightsService.getAllLights().subscribe((lights) => {
+          expect(lights.length).toBe(4);
+          expect(lights[0].name).toEqual('Light 0');
+          expect(lights[1].name).toEqual('Light 1');
+          expect(lights[2].name).toEqual('Light 2');
+          expect(lights[3].name).toEqual('Light 3');
+        });
+    })) // it
+  
+  }) // describe
+
 });
