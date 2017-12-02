93807aa6556db2d1f9442ae38ef4587564322a7d
Sascha Rose <sascha.rose@gmail.com>
2017-12-02 10:26:55 +0100

    modules upgraded

--- src/app/page-components/groups-page/groups-page.component.ts
+++ src/app/page-components/groups-page/groups-page.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupsPageComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+    console.log('ctor');
+
+  }
 
   ngOnInit() {
   }
