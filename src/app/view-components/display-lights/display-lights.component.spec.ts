d9eb791196a7624133712e70c60c48f1f9fde6ce
Sascha Rose <sascha.rose@gmail.com>
2018-01-14 12:06:20 +0100

    fix show details

--- src/app/view-components/display-lights/display-lights.component.spec.ts
+++ src/app/view-components/display-lights/display-lights.component.spec.ts
@@ -2,13 +2,17 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DisplayLightsComponent } from './display-lights.component';
 
+// It is not the best idea to inject the real pipe.
+// TODO: Add a MockPipe to this project.
+import { OrderbyPipe } from '../../pipes/orderby.pipe';
+
 describe('DisplayLightsComponent', () => {
   let component: DisplayLightsComponent;
   let fixture: ComponentFixture<DisplayLightsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DisplayLightsComponent ]
+      declarations: [ DisplayLightsComponent, OrderbyPipe ]
     })
     .compileComponents();
   }));
@@ -22,4 +26,13 @@ describe('DisplayLightsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have switchLight', () => {
+    expect(component.switchLight).toBeTruthy();    
+  });
+
+  it('should have toggleDetails', () => {
+    expect(component.toggleDetails).toBeTruthy();
+  });
+
 });
