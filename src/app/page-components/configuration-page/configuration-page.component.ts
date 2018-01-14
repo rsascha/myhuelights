b8b07e450af3967f72b9eb823561f097d254d314
Sascha Rose <sascha.rose@gmail.com>
2018-01-14 15:49:34 +0100

    it is working, but refactoring needs to be done

--- src/app/page-components/configuration-page/configuration-page.component.ts
+++ src/app/page-components/configuration-page/configuration-page.component.ts
@@ -2,29 +2,25 @@ import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@
 import { HttpClient } from "@angular/common/http";
 import { StorageService } from "app/services/storage.service";
 import { HueConfiguration } from "app/models/hue-configuration";
+import { HueError } from 'app/models/hue-error';
 
 @Component({
   selector: 'app-configuration-page',
   templateUrl: './configuration-page.component.html',
-  styleUrls: ['./configuration-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./configuration-page.component.scss']
 })
 export class ConfigurationPageComponent implements OnInit {
 
-  public alerts: Array<string>;
+  public hueError: HueError;
   public userName: string;
   public hueConfig: HueConfiguration;
-  public ipAddress: string;
 
   constructor(
     private http: HttpClient,
     private storageService: StorageService, 
     private ref: ChangeDetectorRef) {
 
-      this.hueConfig = new HueConfiguration('test', '', '', '');
-      this.hueConfig.ipAddress = 'tt';
-      console.log(this.hueConfig);
-      this.alerts = new Array<string>();
+      this.hueConfig = new HueConfiguration('', 'myhuelights', 'computer', '');
   }
 
   ngOnInit() {
@@ -38,14 +34,9 @@ export class ConfigurationPageComponent implements OnInit {
         .subscribe(response => {
             console.log(response);
             this.hueConfig.ipAddress = response.internalipaddress;
-            this.ipAddress = response.internalipaddress;
-            // https://blog.thoughtram.io/angular/2016/02/22/angular-2-change-detection-explained.html
-            this.alerts.push('test');
-            console.log(this.hueConfig);
             this.ref.markForCheck();
         });
     }
-    console.log(this.hueConfig);
   }
 
   onTestIpAddr(ipAddr: string, appName: string, devName: string): void {
@@ -58,22 +49,24 @@ export class ConfigurationPageComponent implements OnInit {
         response => { 
           console.log('response: ', response)
           if (response.error) {
-            this.alerts.push(response.error.description);
+            this.hueError = response.error;
+            this.ref.markForCheck();
           } else if (response.success) {
-            this.alerts = [];
+            this.hueError = null;
             const userName = response.success.username;
             this.hueConfig = new HueConfiguration(
               ipAddr, appName, devName, userName
             );
-            this.storageService.set(
-              'hue-configuration', 
-              this.hueConfig);
           }
         },
         error => console.log('error: ', error),
         () => console.log('complete')
       );
   }
+
+  saveConfiguration(): void {
+    this.storageService.set('hue-configuration', this.hueConfig);
+}
 
   deleteConfiguration(): void {
     this.storageService.remove('hue-configuration');
